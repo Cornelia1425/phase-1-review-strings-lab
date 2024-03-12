dc0f6f9664304baade00c9effa82dcaf79c8c234
yiranshu <shuyiran1425@hotmail.com>
2024-03-12 13:11:29 -0400

    string

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+//const welcomeMessage = `Welcome to Flatbook, ${currentUser}! `;
+
+// const welcomeMessage = 'Welcome to Flatbook '+ currentUser;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+//or
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
